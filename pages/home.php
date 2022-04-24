0d832011ce017f1cfc474edf4bb896c683799d81
aline.silveira <alinne.1904@gmail.com>
2022-04-24 13:45:38 -0300

    initial commit

---
+++ pages/home.php
@@ -0,0 +1,18 @@
+<?php if (!isset($pagina)) exit; ?>
+
+<h1 class="main-center">
+    Veiculos em Destaque
+</h1>
+<!--colocar os demais itens-->
+<div class="main-grid-destaques">
+    <div class="main-grid-destaques-coluna">
+        <img src="imagens/1.jpg" alt="Kwid">
+        <h2>Renault Kwid</h2>
+        <p>
+            <a href="Kwid" title="Kwid" class="main-btn">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                Detalhes
+            </a>
+        </p>
+    </div>
+</div>
---
+++ pages/kwid.php
@@ -0,0 +1,34 @@
+<?php if (!isset($pagina)) exit; ?>
+<div class="main-grid-veiculo">
+    <div class="main-grid-veiculo-coluna">
+        <a href="imagens/1.jpg" title="Kwid" data-lightbox="Kwid" class="main-foto">
+            <img src="imagens/1.jpg" alt="Kwid">
+        </a>
+    </div>
+    <div class="main-grid-veiculo-coluna">
+        <h1 class="main-center">Renault Kwid</h1>
+        <h2 class="main-center valor">R$ 55.000,00</h2>
+        <div class="main-grid">
+            <div class="main-grid-coluna main-center">
+                <i class="fa-solid fa-calendar-days"></i>
+                <p>2019/2020</p>
+            </div>
+            <div class="main-grid-coluna main-center">
+                <i class="fa-solid fa-calendar-days"></i>
+                <p>2019/2020</p>
+            </div>
+            <div class="main-grid-coluna main-center">
+                <i class="fa-solid fa-calendar-days"></i>
+                <p>2019/2020</p>
+            </div>
+            <div class="main-grid-coluna main-center">
+                <i class="fa-solid fa-calendar-days"></i>
+                <p>2019/2020</p>
+            </div>
+        </div>
+        <p class="main-center">
+            <strong>Descrição</strong>
+        <p>Blá bbláBlá bbláBlá bbláBlá bbláBlá bbláBlá bbláBlá bblá</p>
+        </p>
+    </div>
+</div>
